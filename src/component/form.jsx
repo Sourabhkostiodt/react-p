8400439beb8f591aa20bb1285ae08198424f2a10
Sourabhkostiodt <sourabh.kosti@orangedatatech.com>
2024-05-16 16:25:44 +0530

    detele files

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -112,7 +112,7 @@ function form() {
                onClick={handleSubmit}
               className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
             >
-              Sign in
+              Sign in this
             </button>
             {errorMessage && <p className="error">{errorMessage}</p>}
             
